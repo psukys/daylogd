bb8f861e10a589527783bab24377363aacf521b8
Paulius Šukys <paul.sukys@gmail.com>
2014-09-09 21:52:43 +0300

    Made a working mechanism for running all tests for a directory

--- src/run_tests.py
+++ src/run_tests.py
@@ -1,8 +1,9 @@
 import os
 import unittest
+import re
 
 
-# TODO: find out how to retrieve test cases from a folder
+# Solution provided by http://stackoverflow.com/a/24562019
 def get_test_cases(test_folder):
     """Gets test cases from a folder
 
@@ -12,16 +13,31 @@ def get_test_cases(test_folder):
     Returns:
             testcases that are in test_folder
     """
-    # approximate plan:
-    # 1. Check if file consists of classes
-    # 2. Check if classes are of type unittest.TestCase
-    # 3. if yes - append; else - move to the next class/file
-    pass
+    test_file_names = all_files_in(test_folder)
+    return [path_to_module(str) for str in test_file_names]
+
+
+def all_files_in(test_folder):
+    matches = []
+    for root, dirnames, filenames in os.walk(test_folder):
+        for filename in filenames:
+            if re.match(r"^Test.*.py$", filename):
+                matches.append(os.path.join(root, filename))
+    return matches
+
+
+def path_to_module(filename):
+    while filename.startswith('.'):
+        filename = filename[1:]
+    print filename.replace(".py", "").replace("\\", ".").replace("/", ".")
+    return filename.replace(".py", "").replace("\\", ".").replace("/", ".")
 
 if __name__ == "__main__":
-    test_folder = os.path.abspath("tests")
-    testCases = get_test_cases(test_folder)
+    test_folder = "tests"
+    test_names = get_test_cases(test_folder)
+    test_suites = [unittest.defaultTestLoader.loadTestsFromName(
+        suite_name) for suite_name in test_names]
 
-    for testCase in testCases:
-        suite = unittest.TestLoader().loadTestsFromTestCase(testCase)
-        unittest.TextTestRunner(verbosity=2).run(suite)
+    test_suite = unittest.TestSuite(test_suites)
+    runner = unittest.TextTestRunner(verbosity=1)
+    runner.run(test_suite)
