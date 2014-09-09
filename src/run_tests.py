4d54d26017e2e3fec0dfe01a3d2a1d0b02ebe482
Paulius Šukys <paul.sukys@gmail.com>
2014-09-09 12:24:01 +0300

    Added functionality descriptions also pep8'd

--- src/run_tests.py
+++ src/run_tests.py
@@ -1,13 +1,27 @@
+import os
 import unittest
-from tests import TestDefaults
 
+
+# TODO: find out how to retrieve test cases from a folder
 def get_test_cases(test_folder):
-	pass
+    """Gets test cases from a folder
+
+    Args:
+            test_folder a folder where test_cases should be
+
+    Returns:
+            testcases that are in test_folder
+    """
+    # approximate plan:
+    # 1. Check if file consists of classes
+    # 2. Check if classes are of type unittest.TestCase
+    # 3. if yes - append; else - move to the next class/file
+    pass
 
 if __name__ == "__main__":
-	#TODO: make a function to get the list from tests directory (testcase object)
-	testCases = [TestDefaults.TestDefaults]
-	
-	for testCase in testCases:
-		suite = unittest.TestLoader().loadTestsFromTestCase(testCase)
-		unittest.TextTestRunner(verbosity=2).run(suite)
+    test_folder = os.path.abspath("tests")
+    testCases = get_test_cases(test_folder)
+
+    for testCase in testCases:
+        suite = unittest.TestLoader().loadTestsFromTestCase(testCase)
+        unittest.TextTestRunner(verbosity=2).run(suite)
